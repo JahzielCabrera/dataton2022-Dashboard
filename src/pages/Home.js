ffac656b5754d4418e6ead02772293988d795d0f
Jahziel Cabrera <jahziel_oswaldo@hotmail.com>
2023-02-16 23:57:58 -0600

    New Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,9 @@ export default function Home() {
     return (
         <Box>
             <img width={'100%'} src='https://res.cloudinary.com/djxxgphqp/image/upload/v1670141143/plots/DATOMICOS_imjqce.jpg'/>
+            <Box sx={{display: 'flex'}}>
+                <img height={500} style={{marginLeft: 'auto', marginRight: 'auto'}} src='https://res.cloudinary.com/djxxgphqp/image/upload/v1670187647/plots/qr_datomicos_gu1796.jpg'/>
+            </Box>
         </Box>
     )
 }
